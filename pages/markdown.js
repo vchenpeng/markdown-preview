d84118172269ffd833cb099fdbe6b77f87815942
vchenpeng <vitoc@qq.com>
2017-05-05 12:14:04 +0800

    help
    Signed-off-by: vchenpeng <vitoc@qq.com>

--- pages/markdown.js
+++ pages/markdown.js
@@ -2,6 +2,7 @@
 var uuid = require('node-uuid');
 var Base64 = require('../static/utils/base64.js');
 var CryptoJS = require('../static/utils/aes.js');
+//var aesutil = require('../utils/aesutil.js');
 
 module.exports = function (req, res, app) {
     res.set({
@@ -16,14 +17,30 @@ module.exports = function (req, res, app) {
         fs.exists(url, function (exists) {
             if (exists) {
                 var data = fs.readFileSync(url, "utf-8");
+                console.log('加密前内容',data);
                 var result = CryptoJS.AES.encrypt(data, "aes").toString();
+                //var result = aesutil.encryption("aaabbb", "aes");
                 res.send(result);
             } else {
                 url = 'files/404.md';
                 var data = fs.readFileSync(url, "utf-8");
                 var result = CryptoJS.AES.encrypt(data, "aes").toString();
+                //var result = aesutil.encryption(data, "aes");
                 res.send(result);
             }
+
+            /*if(exists){
+                fs.readFile(url, "utf-8", function(err, data){
+                    var result = CryptoJS.AES.encrypt(data, "aes").toString();
+                    res.send(result);
+                });
+            }else{
+                url = 'files/404.md';
+                fs.readFile(url, "utf-8", function(err, data){
+                    var result = CryptoJS.AES.encrypt(data, "aes").toString();
+                    res.send(result);
+                });
+            }*/
         });
     } else {
         res.send("网页未找到");
